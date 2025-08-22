fda5060e6cf6837701ffb9ee971cb54c2d5a9b9b
CrossVas <cross.wanted.1.4@gmail.com>
2025-08-22 16:01:47 +0300

    added: more info about Molecular Transformer for waila

--- src/main/java/reforged/ic2/addons/asp/compat/waila/SolarPanelInfoProvider.java
+++ src/main/java/reforged/ic2/addons/asp/compat/waila/SolarPanelInfoProvider.java
@@ -1,10 +1,14 @@
 package reforged.ic2.addons.asp.compat.waila;
 
 import mods.vintage.core.helpers.ElectricHelper;
+import mods.vintage.core.platform.lang.Translator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import reforged.ic2.addons.asp.tiles.TileEntityAdvancedSolarPanel;
+import reforged.ic2.addons.asp.tiles.TileEntityMolecularTransformer;
 import reforged.ic2.addons.asp.tiles.TileEntityQuantumGenerator;
+import reforged.ic2.addons.asp.utils.molecular.MTRecipeManager;
+import reforged.ic2.addons.asp.utils.molecular.RecipeRecord;
 import reforged.mods.blockhelper.addons.utils.ColorUtils;
 import reforged.mods.blockhelper.addons.utils.Formatter;
 import reforged.mods.blockhelper.addons.utils.InfoProvider;
@@ -16,6 +20,22 @@ public class SolarPanelInfoProvider extends InfoProvider {
 
     @Override
     public void addInfo(IWailaHelper helper, TileEntity blockeEntity, EntityPlayer player) {
+        if (blockeEntity instanceof TileEntityMolecularTransformer) {
+            TileEntityMolecularTransformer transformer = (TileEntityMolecularTransformer) blockeEntity;
+            text(helper, tier(6));
+            text(helper, maxIn(ElectricHelper.getMaxInputFromTier(6)));
+
+            int lastRecipe = transformer.lastRecipeNumber;
+            if (lastRecipe != -1) {
+                RecipeRecord recipe = MTRecipeManager.instance.getRecipes().get(lastRecipe);
+                text(helper, Translator.GREEN.format("message.info.molecular.input", Translator.WHITE.literal(recipe.input.stackSize + "*") + Translator.AQUA.literal(recipe.input.getDisplayName())));
+                text(helper, Translator.RED.format("message.info.molecular.output", Translator.WHITE.literal(recipe.output.stackSize + "*") + Translator.AQUA.literal(recipe.output.getDisplayName())));
+            }
+            int progress = transformer.lastProgress;
+            if (progress > 0) {
+                bar(helper, transformer.lastProgress, 100, Translator.WHITE.format("message.info.molecular.progress", transformer.lastProgress), ColorUtils.CYAN);
+            }
+        }
         if (blockeEntity instanceof TileEntityQuantumGenerator) {
             TileEntityQuantumGenerator generator = (TileEntityQuantumGenerator) blockeEntity;
             bar(helper, 1, 1, translate("info.energy.infinite"), ColorUtils.RED);
